18c415849f63942f840025cc9e9b7fee4418b641
codejoncode <jonathanjholloway@gmail.com>
2019-03-26 14:47:19 -0500

    added to the followeractions file  added a removefollower function

--- frontend/src/store/actions/FollowersActions.js
+++ frontend/src/store/actions/FollowersActions.js
@@ -1,38 +1,49 @@
-import axios from 'axios'; 
+import axios from "axios";
 
-//helpers 
-import {handleError} from '../../helpers/index.js';
+//helpers
+import { handleError } from "../../helpers/index.js";
 
 const backendURL = process.env.REACT_APP_BACKEND_URL;
 
+/***************************************************************************************************
+ ********************************************* Actions *******************************************
+ **************************************************************************************************/
+export const GET_FOLLOWERS = "GET_FOLLOWERS";
+export const GET_FOLLOWERS_SUCCESS = "GET_FOLLOWERS_SUCCESS";
+export const GET_FOLLOWERS_FAILURE = "GET_FOLLOWERS_FAILURE";
+
+export const ADD_FOLLOWER = "ADD_FOLLOWER";
+export const ADD_FOLLOWER_SUCCESS = "ADD_FOLLOWER_SUCCESS";
+export const ADD_FOLLOWER_FAILURE = "ADD_FOLLOWER_FAILURE";
+
+export const REMOVE_FOLLOWER = "REMOVE_FOLLOWER";
+export const REMOVE_FOLLOWER_SUCCESS = "REMOVE_FOLLOWER_SUCCESS";
+export const REMOVE_FOLLOWER_FAILURE = "REMOVE_FOLLOWER_FAILURE";
 
 /***************************************************************************************************
  ********************************************* Actions *******************************************
  **************************************************************************************************/
-export const GET_FOLLOWERS = 'GET_FOLLOWERS';
-export const GET_FOLLOWERS_SUCCESS = 'GET_FOLLOWERS_SUCCESS';
-export const GET_FOLLOWERS_FAILURE = 'GET_FOLLOWERS_FAILURE';
 
-export const ADD_FOLLOWER = 'ADD_FOLLOWER';
-export const ADD_FOLLOWER_SUCCESS = 'ADD_FOLLOWER_SUCCESS';
-export const ADD_FOLLOWER_FAILURE = 'ADD_FOLLOWER_FAILURE';
-
-export const REMOVE_FOLLOWER = 'REMOVE_FOLLOWER'; 
-export const REMOVE_FOLLOWER_SUCCESS = 'REMOVE_FOLLOWER_SUCCESS';
-export const REMOVE_FOLLOWER_FAILURE = 'REMOVE_FOLLOWER_FAILURE';
-
-
-/***************************************************************************************************
- ********************************************* Actions *******************************************
- **************************************************************************************************/
-
-//GET FOLLOWERS LIST 
+//GET FOLLOWERS LIST
 export const getFollowers = () => dispatch => {
-   const token = localStorage.getItem('symposium_token');
-   const userId = localStoarage.getItem('symposium_user_id');
-   const headers = {headers: {Authorization: token }};
-   dispatch({ type: GET_FOLLOWERS })
-   return axios.get(`${backendURL}/followers/${userId}`, headers)
-        .then(() => dispatch({type: GET_FOLLOWERS_SUCCESS}))
-        .catch(err => handleError(err, GET_FOLLOWERS_FAILURE)(dispatch)); 
-}
+  const token = localStorage.getItem("symposium_token");
+  const userId = localStoarage.getItem("symposium_user_id");
+  const headers = { headers: { Authorization: token } };
+  dispatch({ type: GET_FOLLOWERS });
+  return axios
+    .get(`${backendURL}/followers/${userId}`, headers)
+    .then(() => dispatch({ type: GET_FOLLOWERS_SUCCESS }))
+    .catch(err => handleError(err, GET_FOLLOWERS_FAILURE)(dispatch));
+};
+
+export const removeFollower = following_id => dispatch => {
+  const token = localStorage.getItem("symposium_token");
+  const userId = localStoarage.getItem("symposium_user_id");
+  const headers = { headers: { Authorization: token } };
+  dispatch({type: REMOVE_FOLLOWER})
+  return axios
+    .delete(`${backendURL}/followers/${userId}/${following_id}`, headers)
+    .then(() => dispatch({type: REMOVE_FOLLOWER_SUCCESS}))
+    .catch(err => handleError(err, REMOVE_FOLLOWER_FAILURE)(dispatch));
+};
+
