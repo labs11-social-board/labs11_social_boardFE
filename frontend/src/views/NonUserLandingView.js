3cb966b808f49e5f0ecf26238a9f60e422c9adba
totsuya <supertatortots@gmail.com>
2019-04-04 16:12:26 -0400

    added filler text

--- frontend/src/views/NonUserLandingView.js
+++ frontend/src/views/NonUserLandingView.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import styled from 'styled-components';
-import { connect } from 'react-redux';
-import { background } from '../assets/index.js';
+import {connect} from 'react-redux';
+import {background} from '../assets/index.js';
 
-
-const Woah = styled.section`
+const Woah = styled.section `
   width: 100%;
 <<<<<<< HEAD
   height: 100%;
@@ -21,10 +20,11 @@ const Woah = styled.section`
     background-color: gray;
   }
 
-  ${ ({ loggedIn }) => loggedIn && '@media (max-width: 980px) {background-image: none;background-color: white;}'}
+  ${ ({
+  loggedIn}) => loggedIn && '@media (max-width: 980px) {background-image: none;background-color: white;}'}
 `;
 
-const BackgroundBox = styled.div`
+  const BackgroundBox = styled.div `
   width:100%;
   background: linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)), url(${background});
   background-size: cover;
@@ -32,7 +32,7 @@ const BackgroundBox = styled.div`
   height: 85vh;
 `;
 
-const LandingText = styled.h1`
+  const LandingText = styled.h1 `
   // height: 40%;
   // width: 50%;
   // margin-right: 30px;
@@ -45,7 +45,8 @@ const LandingText = styled.h1`
     background-color: white;
   }
 
-  ${ ({ loggedIn }) => loggedIn && '@media (max-width: 980px) {width: 100%;background-color: white;}'}
+  ${ ({
+    loggedIn}) => loggedIn && '@media (max-width: 980px) {width: 100%;background-color: white;}'}
 
   .blurb{
     font-size: 1.4rem;
@@ -54,81 +55,86 @@ const LandingText = styled.h1`
   }
 `;
 
-// const VideoPlayer = styled.div`
-//   position: relative;
+    // const VideoPlayer = styled.div`   position: relative;   @media (max-width:
+    // 1240px) {     position: absolute;     top: 40%;     left: 0;     right: 0;
+    //  margin-left: auto;     margin-right: auto;   }   ${ ({ loggedIn }) =>
+    // loggedIn && '@media (max-width: 980px) {position: absolute;top: 40%;left:
+    // 0;right: 0;margin-left: auto;margin-right: auto;}'}   .vid-player{     width:
+    // 1400px;     height: 212px;     position: absolute;     top: 0;     right:
+    // 30px;     @media (max-width: 1240px) {       left: 0;       right: 0;
+    // margin-left: auto;       margin-right: auto;     }     ${ ({ loggedIn }) =>
+    // loggedIn && '@media (max-width: 980px) {left: 0;right: 0;margin-left:
+    // auto;margin-right: auto;}'}   } `; const Vidception = styled.div `   width:
+    // 60%;   height: 100%;   padding: 130px;   padding-left: 0px;   `;
 
-//   @media (max-width: 1240px) {
-//     position: absolute;
-//     top: 40%;
-//     left: 0;
-//     right: 0;
-//     margin-left: auto;
-//     margin-right: auto;
-//   }
-
-//   ${ ({ loggedIn }) => loggedIn && '@media (max-width: 980px) {position: absolute;top: 40%;left: 0;right: 0;margin-left: auto;margin-right: auto;}'}
-
-//   .vid-player{
-//     width: 1400px;
-//     height: 212px;
-//     position: absolute;
-//     top: 0;
-//     right: 30px;
-
-//     @media (max-width: 1240px) {
-//       left: 0;
-//       right: 0;
-//       margin-left: auto;
-//       margin-right: auto;
-//     }
-
-//     ${ ({ loggedIn }) => loggedIn && '@media (max-width: 980px) {left: 0;right: 0;margin-left: auto;margin-right: auto;}'}
-//   }
-// `;
-
-// const Vidception = styled.div `
-//   width: 60%;
-//   height: 100%;
-//   padding: 130px;
-//   padding-left: 0px;
-//   `;
-
-  const TopFiller = styled.div`
+    const TopFiller = styled.div `
   
   `;
 
-  const BottomFiller = styled.section`
+    const BottomFiller = styled.section `
+    display: flex;
+    justify-content: space-evenly
     width: 100%;
     border-top: 1px solid gray;
     postion: fixed;
     height: 45vh;
   `;
 
-  
+    const TextBlurb = styled.div `
+    vertical-align: middle;
+    overflow-wrap: break-word;
+    text-align: center;
+    width: 100%;
+    border: 1px solid black;
+  `;
 
+    class NonUserLandingView extends Component {
+      render() {
+        return (
 
-class NonUserLandingView extends Component {
-  render() {
-    return (
+          <Woah loggedIn={this.props.user_id !== 0}>
+            <BackgroundBox>
+              <LandingText loggedIn={this.props.user_id !== 0}>
+                <p className='blurb'>
+                  Welcome to Symposium.
+                </p>
+              </LandingText>
+            </BackgroundBox>
+            <BottomFiller>
+              <TextBlurb>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis
+                  nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident,
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </TextBlurb>
+              <TextBlurb>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                  incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis
+                  nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident,
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+              </TextBlurb>
+              <TextBlurb>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                  incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis
+                  nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident,
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+              </TextBlurb>
+            </BottomFiller>
+          </Woah>
+        )
+      }
+    };
 
-      <Woah loggedIn={this.props.user_id !== 0}>
-      <BackgroundBox>
-        <LandingText loggedIn={this.props.user_id !== 0}>
-          <p className='blurb'>
-            Welcome to Symposium. 
-            </p>
-        </LandingText>
-  </BackgroundBox>
-        <BottomFiller>
-          <p>yeet</p>
-          </BottomFiller>
-      </Woah>
-    )
-  }
-};
+    const mapStateToProps = state => ({user_id: state.users.user_id});
 
-const mapStateToProps = state => ({
-  user_id: state.users.user_id,
-});
-
-export default connect(mapStateToProps, {})(NonUserLandingView);
+    export default connect(mapStateToProps, {})(NonUserLandingView);
