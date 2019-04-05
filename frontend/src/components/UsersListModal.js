d09181240ef8cf965c7fa3531161ec1c6c606db7
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-04-05 13:34:59 -0500

    Added a search users to the team members invite modal instead of showing all users on the site

--- frontend/src/components/UsersListModal.js
+++ frontend/src/components/UsersListModal.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import axios from "axios";
 import styled from 'styled-components';
 
 // action creators
-import { getUsers, addTeamMember } from '../store/actions/index.js';
+import { addTeamMember } from '../store/actions/index.js';
+
+// assets
+import { spinner2 } from "../assets/index.js";
 
 // components 
 import { User } from './index.js';
 
 // globals
-import { phoneL, topHeaderHeight } from '../globals/globals.js';
+import { backendUrl } from "../globals/globals.js";
 
 const ModalBackground = styled.div`
   display: flex;
@@ -61,9 +65,39 @@ overflow: auto;
     }			
   }
 
+  .search-input-wrapper {
+    position: relative;
+    border: none;
+    width: 100%;
+
+    .fa-search {
+      color: #acb1bc;
+      position: absolute;
+      top: 7px;
+      left: 10px;
+    }
+
+    .search-input {
+      width: 80%;
+      border: none;
+      border-radius: 55px;
+      padding: 5px 10px;
+      font-size: 14px;
+      text-indent: 27px;
+      ::placeholder {
+        color: #babec8;
+      }
+    }
+  }
+
 `;
 
 class UsersListModal extends React.Component {
+  state = {
+    searchText: '',
+    loading: false,
+    searchResults: []
+  };
   inviteUser = (e, user_id)=> {
     e.preventDefault();
     const { team_id } = this.props;
@@ -71,18 +105,33 @@ class UsersListModal extends React.Component {
     this.props.addTeamMember(user_id, team_id);
     this.props.setTeamMemberModal(e, false)
   };
-  componentDidMount(){
-    this.props.getUsers();
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value }, () => {
+      this.searchUsers();
+    })
   };
+  searchUsers = () => {
+    const { displayError } = this.props;
+    const { searchText } = this.state;
+    const headers = { headers: { searchText } };
+
+    this.setState({ loading: true }, () => {
+      return axios
+        .get(`${backendUrl}/users/search-user`, headers)
+        .then(res => this.setState({ searchResults: res.data }))
+        .then(() => this.setState({ loading: false }))
+        .catch(err => {
+          const errMsg = err.response
+            ? err.response.data.error
+            : err.toString();
+          return displayError(errMsg).then(() =>
+            this.setState({ loading: false })
+          );
+        });
+    })
+  }
   render(){
-    const { setTeamMemberModal, users, isGettingUsers } = this.props;
-    if(isGettingUsers){
-      return (
-        <ModalBackground>
-          <DivModal>...Loading</DivModal>
-        </ModalBackground>
-      );
-    } else {
+    const { setTeamMemberModal } = this.props;
       return (
         <ModalBackground>
           <DivModalCloser onClick={(e) => setTeamMemberModal(e, false)} />
@@ -94,19 +143,29 @@ class UsersListModal extends React.Component {
               ><i className="far fa-arrow-alt-circle-left"></i></span>
               <span></span>
             </div>
-            <div className='users-list-wrapper'>
-              {users.map(user => <User user={user} key={user.id} inviteUser={this.inviteUser} /> )}
+            <div className='search-input-wrapper'>
+              <span className="fa fa-search" />
+              <input 
+                type='text'
+                name='searchText' 
+                className='search-input' 
+                value={this.state.searchText} 
+                onChange={this.handleInputChange} 
+                placeholder='Search for a User'
+                autocomplete='off'
+              />
             </div>
+            <div className='users-list-wrapper'>
+              {this.state.loading ? <img src={spinner2} alt="spinner" /> 
+                : this.state.searchResults.map(user => <User user={user} key={user.id} inviteUser={this.inviteUser} /> )}
+            </div>
+            
           </DivModal>
         </ModalBackground>
       );
-    }
   };
 };
 
-const mapStateToProps = state => ({
-  users: state.users.users,
-  isGettingUsers: state.users.isGettingUsers
-});
+const mapStateToProps = state => ({ });
 
-export default connect(mapStateToProps, { getUsers, addTeamMember })(UsersListModal);
+export default connect(mapStateToProps, { addTeamMember })(UsersListModal);
