72ebfb4f75a4f73a0b4ffa5b7760b4c5c9b20efe
Imon Ovbude <imonovbude@gmail.com>
2019-04-05 13:30:13 -0700

    updated users.js file

--- frontend/src/views/Users.js
+++ frontend/src/views/Users.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom";
+import {BrowserRouter as Router, Route, Link, NavLink} from "react-router-dom";
 import styled from 'styled-components';
 import { connect } from "react-redux";
 import { getUsers, getUsersNMods, makeMod, makeBas } from './../store/actions/UsersActions';
@@ -35,10 +35,10 @@ class Users extends React.Component {
     constructor() {
         super()
 
-
+        
     }
 
-
+    
 
     componentDidMount() {
         this.props.getUsersNMods();
@@ -50,7 +50,7 @@ class Users extends React.Component {
         this.props.makeMod(userNum);
         setTimeout(() => {
             window.location.reload();
-        }, 800);
+            }, 800);
     }
 
     buttony2 = (event, userNum) => {
@@ -58,54 +58,54 @@ class Users extends React.Component {
         this.props.makeBas(userNum);
         setTimeout(() => {
             window.location.reload();
-        }, 800);
+            }, 800);
     }
-
+    
     render() {
 
 
-        return (
+        return(
             <div>
                 <div >
                     <h4>
-                        <MainWrapper>
-                            <InnerWrapper>Name</InnerWrapper>
-                            <InnerWrapper>E-Mail</InnerWrapper>
-                            <InnerWrapper>Moderator</InnerWrapper>
-                        </MainWrapper>
-                        <hr></hr>
+                    <MainWrapper>
+                    <InnerWrapper>Name</InnerWrapper>
+                    <InnerWrapper>E-Mail</InnerWrapper>
+                    <InnerWrapper>Moderator</InnerWrapper>
+                    </MainWrapper>
+                    <hr></hr>
 
-                        {this.props.users.usersNmods.map(user => {
-                            return (
-
-                                <MainWrapper>
-                                    <InnerWrapper>{user.username}</InnerWrapper>
-                                    <InnerWrapper>{user.email}</InnerWrapper>
-
-                                    <InnerWrapper>
-                                        {
-                                            (user.user_permissions == 'moderator') &&
-                                            <input
-                                                name="isMod"
-                                                type="checkbox"
-                                                checked='true'
-                                                onChange={e => { this.buttony2(e, user.id) }} />
-                                        }
-                                        {
-                                            (user.user_permissions == 'basic') &&
-                                            <input
-                                                name="notMod"
-                                                type="checkbox"
-                                                checked=''
-                                                onChange={e => { this.buttony(e, user.id) }} />
-                                        }
-
-                                    </InnerWrapper>
-                                </MainWrapper>
-
-                            )
-                        })}
-
+                    {this.props.users.usersNmods.map(user =>{
+                        return (
+                            
+                            <MainWrapper>
+                                <InnerWrapper>{user.username}</InnerWrapper>
+                                <InnerWrapper>{user.email}</InnerWrapper>
+                                
+                                <InnerWrapper>
+                                {
+                                    (user.user_permissions == 'moderator') &&
+                                    <input
+                                    name="isMod"
+                                    type="checkbox"
+                                    checked='true'
+                                    onChange={e => {this.buttony2(e, user.id)}} />
+                                }
+                                {
+                                    (user.user_permissions == 'basic') &&
+                                    <input
+                                    name="notMod"
+                                    type="checkbox"
+                                    checked=''
+                                    onChange={e => {this.buttony(e, user.id)}} />
+                                }
+                                
+                                </InnerWrapper>
+                            </MainWrapper>
+                                    
+                        )
+                    })}
+                    
                     </h4>
                 </div>
             </div>
@@ -117,21 +117,8 @@ const mapStateToProps = state => {
     return {
         users: state.users,
     };
-<<<<<<< HEAD
-};
-
-export default connect(
-    mapStateToProps, { getUsers, getUsersNMods }
-)(Users);
-=======
   };
-
+  
   export default connect(
-<<<<<<< HEAD
-    mapStateToProps,{ getUsers, getUsersNMods, makeMod }
+    mapStateToProps,{ getUsers, getUsersNMods, makeMod, makeBas } 
   )(Users);
->>>>>>> 11a2f825ffb0a832eba920ce934d563a7bb2040f
-=======
-    mapStateToProps,{ getUsers, getUsersNMods, makeMod, makeBas }
-  )(Users);
->>>>>>> f0045c44d62b6ba12628f747209effa6cc816270
