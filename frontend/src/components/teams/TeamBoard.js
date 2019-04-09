fe612cd2d46b46883258aaff8cd238038c8a6ccb
totsuya <supertatortots@gmail.com>
2019-04-09 13:24:44 -0400

    updated invite button to be a styled component

--- frontend/src/components/teams/TeamBoard.js
+++ frontend/src/components/teams/TeamBoard.js
@@ -218,6 +218,15 @@ const DiscussionHeader = styled.div`
   }
 `;
 
+const InviteButton = styled.button `
+    border: 1px solid #418DCF;
+    border-radius: 3px;
+    color: white;
+    background-color: #418DCF;
+    height: 35px;
+    width: 50px;
+`;
+
 const newest = 'newest';
 const oldest = 'oldest';
 const mostUpvotes = 'most upvotes';
@@ -358,7 +367,7 @@ class TeamBoard extends Component {
                 historyPush={history.push}
                 team_members={team_members}
               />
-              {!isMember ? null : isTeamMembersTab ? <button onClick={e => this.setTeamMemberModal(e, true)}>Invite</button> : null}
+              {!isMember ? null : isTeamMembersTab ? <InviteButton onClick={e => this.setTeamMemberModal(e, true)}>Invite</InviteButton> : null}
             </div>
             <div className = 'team-tabs'>
               <h3 className='tab tab-selected' onClick={this.handleTab}>Discussions</h3>
