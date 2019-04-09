d84e179b6f281ad186dab3e9d643af9e5408b39b
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-04-09 18:28:24 -0500

    the drag and drop feature is now responsive when posting a comment

--- frontend/src/components/forms/AddPostForm.js
+++ frontend/src/components/forms/AddPostForm.js
@@ -90,6 +90,26 @@ const UserActions = styled.div`
     color: black;
     text-decoration: none;
   }
+
+  .drag-zone-wrapper {
+    bottom: 34%;
+    height: 3.5vh;
+
+    @media(max-width: 1440px){
+      bottom: 34%;
+      height: 4vh;
+    }
+
+    @media(max-width: 1024px){
+      bottom: 35.6%;
+    }
+
+    @media ${phoneP}{
+      bottom: 33.7%;
+      height: 5vh;
+      width: 39%
+    }
+  }
 `;
 
 class AddPostForm extends Component {
