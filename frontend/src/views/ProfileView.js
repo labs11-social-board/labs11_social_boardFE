32d352601865c77f7a9e24eca9111eb9fcf46e9d
codejoncode <jonathanjholloway@gmail.com>
2019-04-11 11:57:23 -0500

    removed the margin on the button

--- frontend/src/views/ProfileView.js
+++ frontend/src/views/ProfileView.js
@@ -193,7 +193,6 @@ color: ${props => props.theme.defaultColor};
 `;
 
 const Button = styled.button`
-  margin-left: 10px;
   padding: 10px 15px;
   border-radius: 5px;
   border: 1px solid #418DCF;
