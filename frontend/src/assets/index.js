2fc98383f65a010a3fcbcd00f90aa5659ae89294
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-04-17 10:56:30 -0500

    Removed import/export of file since it was not being used and adjusted text for image uploading

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -5,7 +5,6 @@ import socrates from './img/socrates.png';
 import landingPageImage1 from './img/landing-page-image1.png';
 import background from './img/background.jpg';
 import computericon from './img/computericon.png';
-import example from './img/example.png';
 import subscriptions from './img/subscriptions.png'
 
 export {
@@ -16,6 +15,5 @@ export {
 	landingPageImage1,
 	background,
 	computericon,
-	example,
 	subscriptions
 };
