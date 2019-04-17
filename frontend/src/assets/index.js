7f5f9e4e832c26cb512adc247095983ce6cc0dc1
mag16 <marcoguzman16@gmail.com>
2019-04-17 08:57:39 -0700

    quick spell fix on Non user landing page word discussions

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -5,7 +5,7 @@ import socrates from './img/socrates.png';
 import landingPageImage1 from './img/landing-page-image1.png';
 import background from './img/background.jpg';
 import computericon from './img/computericon.png';
-import example from './img/example.png';
+import example from './img/example.PNG';
 import subscriptions from './img/subscriptions.png'
 
 export {
