163f583e908006037ffc4f5e8050528df6483c06
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-04-18 15:29:30 -0500

    removed unneeded code

--- frontend/src/components/DiscussionsByCats.js
+++ frontend/src/components/DiscussionsByCats.js
@@ -248,67 +248,8 @@ class DiscussionsByCats extends Component {
     }
   }
   render() {
-    const { discussions, history, category_name, match, isGettingDiscussions } = this.props;
-    const { showAddDiscussionForm } = this.state;
-    return ( this.conditionalRender()
-      // <>
-      //   {isGettingDiscussions ? <img src={require('../assets/gif/spinner2.gif')} alt='spinner'/>
-      //   :
-      //     <DiscussionsWrapper>
-      //       <DiscussionHeader>
-      //         <div className='name-follow-wrapper'>
-      //           <h2 className='name'>{category_name}</h2>
-      //           <FollowCat
-      //             category_id={match.params.category_id}
-      //             historyPush={history.push}
-      //           />
-      //         </div>
-      //         <div className='filter-add-btn-wrapper'>
-      //           <div className='filter-wrapper'>
-      //             <i className='fab fa-mix' />
-      //             <span>Filter by</span>
-      //             <select
-      //               className='filter'
-      //               onChange={this.handleSelectChange}
-      //               name='filter'
-      //             >
-      //               <option value={newest}>{newest}</option>
-      //               <option value={oldest}>{oldest}</option>
-      //               <option value={mostUpvotes}>{mostUpvotes}</option>
-      //               <option value={mostViews}>{mostViews}</option>
-      //               <option value={mostComments}>{mostComments}</option>
-      //             </select>
-      //           </div>
-      //           <button onClick={this.toggleAddDiscussionForm} className='add-post-btn'>
-      //             <i className='fas fa-plus-circle' />&nbsp;Add Post
-      //           </button>
-      //         </div>
-      //       </DiscussionHeader>
-      //       <hr />
-      //       <div className='content'>
-      //         {isGettingDiscussions ? <img src={require('../assets/gif/spinner2.gif')} alt='spinner'/> : discussions.map((discussion, i) =>
-      //           <DiscussionByFollowedCats
-      //             key={i}
-      //             discussion={discussion}
-      //             history={history}
-      //             voteOnDiscussion={this.handleDiscussionVote}
-      //             isShowImage={this.state.isShowImage}
-      //             handleImageShow={this.handleImageShow}
-      //             imageClickedId={this.state.imageClickedId}
-      //           />)
-      //         }
-      //       </div>
-      //       {
-      //         showAddDiscussionForm &&
-      //         <AddDiscussionForm
-      //           toggleAddDiscussionForm={this.toggleAddDiscussionForm}
-      //           getDiscussions={this.getDiscussions}
-      //           category_id={match.params.category_id}
-      //         />
-      //       }
-      //     </DiscussionsWrapper>
-      //   }
-      // </>
+    return ( 
+      this.conditionalRender()
     );
   }
 };
