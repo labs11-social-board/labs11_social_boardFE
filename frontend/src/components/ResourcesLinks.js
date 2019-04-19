c2c496390c98b1912478a319bce89761aebd0b82
amberm <nekosenpai@gmail.com>
2019-04-19 14:00:47 -0600

    fixing the things, all the things

--- frontend/src/components/ResourcesLinks.js
+++ frontend/src/components/ResourcesLinks.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const LinkResource = styled.a`
 border-left: ${props => props.islinkselected === 'true' ? `5px solid ${props.theme.defaultColorOnHover}` : '5px solid transparent'};
-//border-left: 5px solid ${props => props.theme.defaultColorOnHover};
+//border-left: 25px solid ${props => props.theme.defaultColorOnHover};
   text-decoration: none;
   // color: ${props => props.islinkselected === 'true' ? 'blue' : 'black'};
   color: ${props => props.islinkselected === 'true' ? `${props.theme.defaultColorOnHover}` : `${props.theme.defaultColor}`};
@@ -14,12 +14,12 @@ border-left: ${props => props.islinkselected === 'true' ? `5px solid ${props.the
     width: 46px;
     display: inline-block;
     text-align: center;
-    margin: 0;
+    margin-left: 27px;
     i {
       cursor: pointer;
       padding: 10px 10px 10px 0;
       color: inherit;
-      margin-left: 15px;
+      margin-left: 35px;
     }
   }
   &:hover {
