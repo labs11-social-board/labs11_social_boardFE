cb817470095426066105741793837494851a8db3
Imon Ovbude <imonovbude@gmail.com>
2019-04-24 16:05:30 -0700

    changed dislikes

--- frontend/src/components/ApprovedEmails.js
+++ frontend/src/components/ApprovedEmails.js
@@ -1,37 +1,28 @@
-import React, {
-    Component
-} from 'react';
+import React, { Component } from "react";
 
-import {
-    connect
-} from 'react-redux';
+import { connect } from "react-redux";
 
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import {
-    approveEmail,
-    getEmails,
-    denyEmail
-} from '../store/actions';
+import { approveEmail, getEmails, denyEmail } from "../store/actions";
 
 import ReactTable from "react-table";
 import "react-table/react-table.css";
 
 const DenyButton = styled.button`
-    border: 1px solid red;
-    border-radius: 3px;
-    background-color: red;
-    color: white;
-    font-weight: bold;
-    height: 35px;
-    width: 100px;
-    margin-left: 4px;
-    cursor: pointer;
-
-`
+  border: 1px solid red;
+  border-radius: 3px;
+  background-color: red;
+  color: white;
+  font-weight: bold;
+  height: 35px;
+  width: 100px;
+  margin-left: 4px;
+  cursor: pointer;
+`;
 const SubDrawer = styled.div`
   padding: 20px;
-`
+`;
 
 const MainWrapper = styled.div`
   display: flex;
@@ -47,132 +38,124 @@ const MainWrapper = styled.div`
     margin-top: 30px;
     margin-bottom: 5px;
   }
-
 `;
 
 const InnerWrapper = styled.div`
-width: 90%;
-font-size: 1.1rem;
-justify-content: space-around;
-justify-items: center;
-align-items: center;
-align-content: center;
+  width: 90%;
+  font-size: 1.1rem;
+  justify-content: space-around;
+  justify-items: center;
+  align-items: center;
+  align-content: center;
 `;
 
 const Boxed = styled.div`
-width: 100%;
-padding: 10px;
-border: 1px solid #418dcf;
-border-radius: 5px;
-flex-direction: column;
-color:black;
-justify-content: space-around;
-margin-bottom: 10px;
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #418dcf;
+  border-radius: 5px;
+  flex-direction: column;
+  color: black;
+  justify-content: space-around;
+  margin-bottom: 10px;
 `;
 
 const TableWrapper = styled.div`
-width: 90%;
+  width: 90%;
 `;
 
-
-
-
 class ApprovedEmails extends Component {
-    componentDidMount() {
-        this.props.getEmails();
-        this.addFilterPlaceholder();
-    }
+  componentDidMount() {
+    this.props.getEmails();
+    this.addFilterPlaceholder();
+  }
 
-    handleClick(e, id) {
-        e.preventDefault();
+  handleClick(e, id) {
+    e.preventDefault();
 
-        this.props.denyEmail(id)
-        setTimeout(() => {
-            window.location.reload();
-        }, 400);
-    }
+    this.props.denyEmail(id);
+    setTimeout(() => {
+      window.location.reload();
+    }, 400);
+  }
 
-    addFilterPlaceholder = () => {
-        const filters = document.querySelectorAll("div.rt-th > input");
-        for (let filter of filters) {
-            filter.placeholder = "Search...";
-        }
+  addFilterPlaceholder = () => {
+    const filters = document.querySelectorAll("div.rt-th > input");
+    for (let filter of filters) {
+      filter.placeholder = "Search...";
     }
+  };
 
-    render() {
-        return (
-            <TableWrapper>
-                <MainWrapper>
-                <InnerWrapper>
-                <Boxed>
-                    <h4>Approved E-Mails Table View</h4>
-                <ReactTable
-                    data={this.props.approvedEmails}
-                    filterable
-                    defaultFilterMethod={
-                        (filter, row) =>
-                            String(row[filter.id]) === filter.includes(filter.value)
-                    }
-                    columns={
-                        [
-                            {
-                                Header: "Approved E-Mails",
-                                accessor: "email",
-                                filterMethod: (filter, row) =>
-                                    row[filter.id].startsWith(filter.value) &&
-                                    row[filter.id].endsWith(filter.value)
-                            }
-                        ]
-                    }
-                    SubComponent={
-                        row => {
-                            console.log(row)
-                            return (
-                                <SubDrawer>
-                                    <DenyButton
-                                        onClick={e => this.handleClick(e, row.original.id)}
-                                    > Deny Email </DenyButton>
-                                </SubDrawer>
-                            );
-                        }
-                    }
-                    defaultPageSize={
-                        5
-                    }
-                    className="-striped -highlight"
-                    style={
-                        this.props.isDay ?
-                            {
-                                color: 'black'
-                            }
-                            : {
-                                color: 'black'
-                            }
-                    }
-                />
-                </Boxed>
-                </InnerWrapper>
-                </MainWrapper>
-            </TableWrapper>
-        )
-    }
+  render() {
+    return (
+      <TableWrapper>
+        <MainWrapper>
+          <InnerWrapper>
+            <Boxed>
+              <h4>Approved E-Mails Table View</h4>
+              <ReactTable
+                data={this.props.approvedEmails}
+                filterable
+                defaultFilterMethod={(filter, row) =>
+                  String(row[filter.id]) === filter.includes(filter.value)
+                }
+                columns={[
+                  {
+                    Header: "Approved E-Mails",
+                    accessor: "email",
+                    filterMethod: (filter, row) =>
+                      row[filter.id].startsWith(filter.value) &&
+                      row[filter.id].endsWith(filter.value)
+                  }
+                ]}
+                SubComponent={row => {
+                  console.log(row);
+                  return (
+                    <SubDrawer>
+                      <DenyButton
+                        onClick={e => this.handleClick(e, row.original.id)}
+                      >
+                        {" "}
+                        Deny Email{" "}
+                      </DenyButton>
+                    </SubDrawer>
+                  );
+                }}
+                defaultPageSize={5}
+                className="-striped -highlight"
+                style={
+                  this.props.isDay
+                    ? {
+                        color: "black"
+                      }
+                    : {
+                        color: "black"
+                      }
+                }
+              />
+            </Boxed>
+          </InnerWrapper>
+        </MainWrapper>
+      </TableWrapper>
+    );
+  }
 }
 
 const mapStoreToProps = state => {
-    return {
-        approvedEmails: state.emails.approvedEmails
-    }
-}
+  return {
+    approvedEmails: state.emails.approvedEmails
+  };
+};
 
 const mapDispatchToProps = dispatch => {
-    return {
-        approveEmail: email => dispatch(approveEmail(email)),
-        getEmails: () => dispatch(getEmails()),
-        denyEmail: id => dispatch(denyEmail(id))
-    }
-}
+  return {
+    approveEmail: email => dispatch(approveEmail(email)),
+    getEmails: () => dispatch(getEmails()),
+    denyEmail: id => dispatch(denyEmail(id))
+  };
+};
 
 export default connect(
-    mapStoreToProps,
-    mapDispatchToProps
+  mapStoreToProps,
+  mapDispatchToProps
 )(ApprovedEmails);
--- frontend/src/views/Users.js
+++ frontend/src/views/Users.js
@@ -1,71 +1,74 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import styled from "styled-components";
 import { connect } from "react-redux";
-import { getUsers, getUsersNMods, makeMod, makeBas } from './../store/actions/UsersActions';
-import {getEmails, approveEmail, denyEmail} from './../store/actions'
+import {
+  getUsers,
+  getUsersNMods,
+  makeMod,
+  makeBas
+} from "./../store/actions/UsersActions";
+import { getEmails, approveEmail, denyEmail } from "./../store/actions";
 import ReactTable from "react-table";
 import "react-table/react-table.css";
 
 const SubDrawer = styled.div`
   padding: 20px;
   display: flex;
-`
+`;
 
 const Button = styled.button`
-    border: 1px solid #418DCF;
-    border-radius: 3px;
-    color: white;
-    background-color: #418DCF;
-    height: 35px;
-    width: 100px;
-    margin-left: 4px;
-    cursor: pointer;
-    margin-right: 20px;
-`
+  border: 1px solid #418dcf;
+  border-radius: 3px;
+  color: white;
+  background-color: #418dcf;
+  height: 35px;
+  width: 100px;
+  margin-left: 4px;
+  cursor: pointer;
+  margin-right: 20px;
+`;
 
 class Users extends Component {
-    componentDidMount() {
-        this.props.getUsersNMods();
-        this.props.getEmails();
+  componentDidMount() {
+    this.props.getUsersNMods();
+    this.props.getEmails();
+  }
+
+  buttony = (event, userNum) => {
+    event.preventDefault();
+    this.props.makeMod(userNum);
+    setTimeout(() => {
+      window.location.reload();
+    }, 800);
+  };
+
+  buttony2 = (event, userNum) => {
+    event.preventDefault();
+    this.props.makeBas(userNum);
+    setTimeout(() => {
+      window.location.reload();
+    }, 800);
+  };
+
+  handleEmailToggle(event, email) {
+    event.preventDefault();
+
+    if (this.props.approvedEmails.map(e => e.email).includes(email)) {
+      this.props.approvedEmails.map(e => {
+        if (e.email === email) {
+          this.props.denyEmail(e.id);
+        }
+      });
+    } else {
+      this.props.approveEmail({ email });
     }
 
-    buttony = (event, userNum) => {
-        event.preventDefault();
-        this.props.makeMod(userNum);
-        setTimeout(() => {
-            window.location.reload();
-        }, 800);
-    }
+    setTimeout(() => {
+      window.location.reload();
+    }, 800);
+  }
 
-    buttony2 = (event, userNum) => {
-        event.preventDefault();
-        this.props.makeBas(userNum);
-        setTimeout(() => {
-            window.location.reload();
-        }, 800);
-    }
-    
-    handleEmailToggle(event, email){
-
-      event.preventDefault();
-  
-      if (this.props.approvedEmails.map(e => e.email).includes(email)) {
-        this.props.approvedEmails.map(e => {
-          if (e.email === email) {
-            this.props.denyEmail(e.id)
-          }
-        })
-      } else {
-        this.props.approveEmail({email})
-      }
-    
-      setTimeout(() => {
-        window.location.reload();
-      }, 800);
-
-    }
-    
-    render() {
+  render() {
     return (
       <div>
         <ReactTable
@@ -74,43 +77,37 @@ class Users extends Component {
           defaultFilterMethod={(filter, row) =>
             String(row[filter.id]) === filter.value
           }
-          SubComponent = {
-            row => {
-              console.log(row)
-              return ( 
-                <SubDrawer>
-
-                  {
-                    row.original.user_permissions === 'moderator' ?
-                      <Button
-                      onClick = {
-                          e => {
-                            this.buttony2(e, row.original.id);
-                          }
-                        } >
-                        Make Basic 
-                        </Button>
-                    :
-                      <Button
-                      onClick = {
-                          e => {
-                            this.buttony(e, row.original.id);
-                          }
-                        }>
-                        Make Moderator 
-                        </Button>
-                  }
+          SubComponent={row => {
+            console.log(row);
+            return (
+              <SubDrawer>
+                {row.original.user_permissions === "moderator" ? (
                   <Button
-                  onClick = {
-                      e => {
-                        this.handleEmailToggle(e, row.original.email)
-                      }
-                    }>
-                    Toggle E-mail Status</Button>
-                </SubDrawer>
-              );
-            }
-          }
+                    onClick={e => {
+                      this.buttony2(e, row.original.id);
+                    }}
+                  >
+                    Make Basic
+                  </Button>
+                ) : (
+                  <Button
+                    onClick={e => {
+                      this.buttony(e, row.original.id);
+                    }}
+                  >
+                    Make Moderator
+                  </Button>
+                )}
+                <Button
+                  onClick={e => {
+                    this.handleEmailToggle(e, row.original.email);
+                  }}
+                >
+                  Toggle E-mail Status
+                </Button>
+              </SubDrawer>
+            );
+          }}
           columns={[
             {
               Header: "Username",
@@ -153,12 +150,11 @@ class Users extends Component {
             {
               Header: "E-Mail Status",
               id: "email_status",
-              accessor: (u => {
-
+              accessor: u => {
                 let emails = this.props.approvedEmails.map(e => e.email);
 
-                return emails.includes(u.email) ? 'Approved' : 'Denied'
-              }),
+                return emails.includes(u.email) ? "Approved" : "Denied";
+              },
               filterMethod: (filter, row) => {
                 if (filter.value === "all") {
                   return true;
@@ -168,62 +164,65 @@ class Users extends Component {
                 }
                 return row[filter.id] === "Denied";
               },
-              Filter: ({
-                filter,
-                onChange
-              }) => ( 
-                <select onChange = {
-                  event => onChange(event.target.value)
-                }
-                style = {
-                  {
+              Filter: ({ filter, onChange }) => (
+                <select
+                  onChange={event => onChange(event.target.value)}
+                  style={{
                     width: "100%"
-                  }
-                }
-                value = {
-                  filter ? filter.value : "all"
-                } >
-                <option value = "all"> Show All </option> 
-                <option value = "true"> Approved </option> 
-                <option value = "false" > Denied </option> 
+                  }}
+                  value={filter ? filter.value : "all"}
+                >
+                  <option value="all"> Show All </option>
+                  <option value="true"> Approved </option>
+                  <option value="false"> Denied </option>
                 </select>
               )
             }
           ]}
           defaultPageSize={10}
           className="-striped -highlight"
-          style = {
-            this.props.isDay ? {
-              color: 'black'
-            } :
-            {
-              color: 'white'
-            }
+          style={
+            this.props.isDay
+              ? {
+                  color: "black"
+                }
+              : {
+                  color: "white"
+                }
           }
         />
       </div>
-    ); 
-    }
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        users: state.users,
-        approvedEmails: state.emails.approvedEmails,
-        approvingEmail: state.emails.approvingEmail,
-        deletingEmail: state.emails.deletingEmail,
-        fetchingEmails: state.emails.fetchingEmails
-    };
+  return {
+    users: state.users,
+    approvedEmails: state.emails.approvedEmails,
+    approvingEmail: state.emails.approvingEmail,
+    deletingEmail: state.emails.deletingEmail,
+    fetchingEmails: state.emails.fetchingEmails
   };
+};
 
-  // const mapDispatchToProps = dispatch => {
-  //   return {
-  //     approveEmail: email => dispatch(approveEmail(email)),
-  //     getEmails: () => dispatch(getEmails()),
-  //     denyEmail: 
-  //   }
-  // }
-  
-  export default connect(
-    mapStateToProps,{ getUsers, getUsersNMods, makeMod, makeBas, getEmails, approveEmail, denyEmail } 
-  )(Users);
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     approveEmail: email => dispatch(approveEmail(email)),
+//     getEmails: () => dispatch(getEmails()),
+//     denyEmail:
+//   }
+// }
+
+export default connect(
+  mapStateToProps,
+  {
+    getUsers,
+    getUsersNMods,
+    makeMod,
+    makeBas,
+    getEmails,
+    approveEmail,
+    denyEmail
+  }
+)(Users);
